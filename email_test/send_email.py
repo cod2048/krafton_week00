168a9699ab9d0c337d3885b55c9c1f2442d3aaed
cod2048 <cod2048@gmail.com>
2023-04-11 21:28:43 +0900

    이메일 수정

--- email_test/send_email.py
+++ email_test/send_email.py
@@ -8,17 +8,17 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
 
 #로그인 인증
-s.login('hawk58927@gmail.com', 'agqhlglabmfjihpo')
+s.login('email', 'pwd')
 
 #보낼 메시지 설정
 msg = MIMEText("내용 : 본문내용 테스트입니다.")
 msg['Subject'] = '제목 : 메일 보내기 테스트입니다.'
-msg['From'] = 'hawk58927@gmail.com'
-msg['To'] = 'cod2048@gmail.com'
+msg['From'] = '@gmail.com'
+msg['To'] = '@gmail.com'
 
 
 #메일보내기
-s.sendmail("hawk58927@gmail.com", "cod2048@gmail.com", msg.as_string())
+s.sendmail("@gmail.com", "@gmail.com", msg.as_string())
 
 #세션 종료
 s.quit()
